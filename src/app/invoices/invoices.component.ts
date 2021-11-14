c585944b714df3ebd4944e84a23f9cd326a88abc
uppu540 <upendra.giduturi540@outlook.com>
2021-11-14 19:34:40 +0530

    updated changes

--- src/app/invoices/invoices.component.ts
+++ src/app/invoices/invoices.component.ts
@@ -6,10 +6,84 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./invoices.component.styl']
 })
 export class InvoicesComponent implements OnInit {
-
+  value: Date;
+  orderList: any = [];
+  showType:any = 'default'
   constructor() { }
 
   ngOnInit() {
+
+    this.orderList = [
+      {
+        invoiceNo: 'DM/UP/82',
+        manufac: 'Angel Broking Ltd',
+        orderDate: '30-Jul-2021',
+        delvDare: '30-Jul-2021',
+        mop: 'Credit',
+        amount: '1,27,735.94'
+      },
+      {
+        invoiceNo: 'DM/UP/82',
+        manufac: 'Angel Broking Ltd',
+        orderDate: '30-Jul-2021',
+        delvDare: '30-Jul-2021',
+        mop: 'Credit',
+        amount: '1,27,735.94'
+      },
+      {
+        invoiceNo: 'DM/UP/82',
+        manufac: 'Angel Broking Ltd',
+        orderDate: '30-Jul-2021',
+        delvDare: '30-Jul-2021',
+        mop: 'Credit',
+        amount: '1,27,735.94'
+      },
+      {
+        invoiceNo: 'DM/UP/82',
+        manufac: 'Angel Broking Ltd',
+        orderDate: '30-Jul-2021',
+        delvDare: '30-Jul-2021',
+        mop: 'Credit',
+        amount: '1,27,735.94'
+      },
+      {
+        invoiceNo: 'DM/UP/82',
+        manufac: 'Angel Broking Ltd',
+        orderDate: '30-Jul-2021',
+        delvDare: '30-Jul-2021',
+        mop: 'Credit',
+        amount: '1,27,735.94'
+      },
+      {
+        invoiceNo: 'DM/UP/82',
+        manufac: 'Angel Broking Ltd',
+        orderDate: '30-Jul-2021',
+        delvDare: '30-Jul-2021',
+        mop: 'Credit',
+        amount: '1,27,735.94'
+      },
+      {
+        invoiceNo: 'DM/UP/82',
+        manufac: 'Angel Broking Ltd',
+        orderDate: '30-Jul-2021',
+        delvDare: '30-Jul-2021',
+        mop: 'Credit',
+        amount: '1,27,735.94'
+      },
+      {
+        invoiceNo: 'DM/UP/82',
+        manufac: 'Angel Broking Ltd',
+        orderDate: '30-Jul-2021',
+        delvDare: '30-Jul-2021',
+        mop: 'Credit',
+        amount: '1,27,735.94'
+      },
+    ]
+
+  }
+
+  openType(value: any) {
+    this.showType = value;
   }
 
 }
--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -6,10 +6,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orders.component.styl']
 })
 export class OrdersComponent implements OnInit {
-
+  value: Date;
+  orderList: any = [];
   constructor() { }
 
   ngOnInit() {
+    this.orderList = [
+      {
+        orderNo: '123456789',
+        manufac: 'Angel Broking Ltd',
+        orderDate: '30-Jul-2021',
+        challan: 'DM/UP/82',
+        status: 1
+      },
+      {
+        orderNo: '123456789',
+        manufac: 'Angel Broking Ltd',
+        orderDate: '30-Jul-2021',
+        challan: 'DM/UP/82',
+        status: 2
+      },
+      {
+        orderNo: '123456789',
+        manufac: 'Angel Broking Ltd',
+        orderDate: '30-Jul-2021',
+        challan: 'DM/UP/82',
+        status: 3
+      },
+      {
+        orderNo: '123456789',
+        manufac: 'Angel Broking Ltd',
+        orderDate: '30-Jul-2021',
+        challan: 'DM/UP/82',
+        status: 4
+      },
+      {
+        orderNo: '123456789',
+        manufac: 'Angel Broking Ltd',
+        orderDate: '30-Jul-2021',
+        challan: 'DM/UP/82',
+        status: 1
+      },
+      {
+        orderNo: '123456789',
+        manufac: 'Angel Broking Ltd',
+        orderDate: '30-Jul-2021',
+        challan: 'DM/UP/82',
+        status: 1
+      },
+      {
+        orderNo: '123456789',
+        manufac: 'Angel Broking Ltd',
+        orderDate: '30-Jul-2021',
+        challan: 'DM/UP/82',
+        status: 2
+      },
+      {
+        orderNo: '123456789',
+        manufac: 'Angel Broking Ltd',
+        orderDate: '30-Jul-2021',
+        challan: 'DM/UP/82',
+        status: 3
+      },
+    ]
+
   }
 
 }
